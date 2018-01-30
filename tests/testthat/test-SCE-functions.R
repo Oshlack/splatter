4f6a4fcaae707ea583ce28039c564f8899f27f30
Luke Zappia <lazappi@users.noreply.github.com>
2018-01-30 17:53:53 +1100

    Add tests

--- tests/testthat/test-SCE-functions.R
+++ tests/testthat/test-SCE-functions.R
@@ -1,7 +1,26 @@
 context("SCE functions")
 
+sce <- simpleSimulate()
+
+test_that("addFeatureStats works with counts", {
+    ss <- addFeatureStats(sce)
+    expect_true(all(c("MeanCounts", "VarCounts", "CVCounts", "MedCounts",
+                      "MADCounts") %in% colnames(rowData(ss))))
+    ss <- addFeatureStats(sce, log = TRUE)
+    expect_true(all(c("MeanLogCounts", "VarLogCounts", "CVLogCounts",
+                      "MedLogCounts", "MADLogCounts") %in%
+                    colnames(rowData(ss))))
+    ss <- addFeatureStats(sce, no.zeros = TRUE)
+    expect_true(all(c("MeanCountsNo0", "VarCountsNo0", "CVCountsNo0",
+                      "MedCountsNo0", "MADCountsNo0") %in%
+                        colnames(rowData(ss))))
+    ss <- addFeatureStats(sce, log = TRUE, no.zeros = TRUE)
+    expect_true(all(c("MeanLogCountsNo0", "VarLogCountsNo0", "CVLogCountsNo0",
+                      "MedLogCountsNo0", "MADLogCountsNo0") %in%
+                        colnames(rowData(ss))))
+})
+
 test_that("addGeneLengths generate method works", {
-    sce <- simpleSimulate()
     expect_silent(addGeneLengths(sce))
     expect_error(addGeneLengths("a"))
     expect_error(addGeneLengths(sce, loc = "a"))
@@ -10,7 +29,6 @@ test_that("addGeneLengths generate method works", {
 })
 
 test_that("addGeneLength sample method works", {
-    sce <- simpleSimulate()
     lens <- round(runif(100, 100, 10000))
     expect_silent(addGeneLengths(sce, method = "sample", lengths = lens))
     expect_error(addGeneLengths(sce, method = "sample"))
