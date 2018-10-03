eb22e79dcbacae5c3e095583abadadeae65b1114
Luke Zappia <lazappi@users.noreply.github.com>
2018-10-03 14:25:37 +1000

    Fix BASiCSEstimate tests

--- tests/testthat/test-BASiCSEstimate.R
+++ tests/testthat/test-BASiCSEstimate.R
@@ -9,7 +9,7 @@ test_that("BASiCSEstimate works", {
     spike.info <- data.frame(Name = rownames(sc_example_counts)[1:10],
                              Input = rnorm(10, 500, 200),
                              stringsAsFactors = FALSE)
-    counts <- sc_example_counts[, 1:20]
+    counts <- sc_example_counts[, 1:30]
     counts <- counts[rowSums(counts) != 0, ]
     params <- BASiCSEstimate(counts[1:100, ],
                              spike.info, verbose = FALSE, progress = FALSE)
@@ -19,7 +19,7 @@ test_that("BASiCSEstimate works", {
 test_that("BASiCSEstimate works without spikes", {
     skip_if_not_installed("BASiCS")
     set.seed(1)
-    counts <- sc_example_counts[, 1:20]
+    counts <- sc_example_counts[, 1:30]
     counts <- counts[rowSums(counts) != 0, ]
     batch <- sample(1:2, ncol(counts), replace = TRUE)
     params <- BASiCSEstimate(counts[1:100, ], batch = batch,
