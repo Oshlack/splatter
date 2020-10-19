14fd3be80f9d7bf77bff79c919612ada2815a388
Luke Zappia <lazappi@users.noreply.github.com>
2020-10-19 13:38:59 +0200

    Pass params to splatEstimate() in splatPopeEstimate()

--- R/splatPop-estimate.R
+++ R/splatPop-estimate.R
@@ -2,44 +2,47 @@
 #'
 #' Estimate simulation parameters for the eQTL population simulation from
 #' real data. See the individual estimation functions for more details on
-#' how this is done. 
+#' how this is done.
 #'
-#' @param params splatPopParams object containing parameters for the 
+#' @param params splatPopParams object containing parameters for the
 #'         simulation of the mean expression levels for the population.
 #'        See \code{\link{splatPopParams}} for details.
 #' @param counts either a counts matrix or a SingleCellExperiment object
 #'        containing count data to estimate parameters from.
-#' @param means Matrix of real gene means across a population, where 
+#' @param means Matrix of real gene means across a population, where
 #'        each row is a gene and each column is an individual in the population.
 #' @param eqtl Data.frame with all or top eQTL pairs from a real eQTL analysis.
 #'         Must include columns: 'gene_id', 'pval_nominal', and 'slope'.
-#' 
+#'
 #' @seealso
 #' \code{\link{splatPopEstimateEffectSize}},
 #' \code{\link{splatPopEstimateMeanCV}}
 #'
 #' @return splatPopParams object containing the estimated parameters.
-#' 
+#'
 #' @export
-#' 
-splatPopEstimate <- function(params = newSplatPopParams(), 
-                             counts = NULL,
-                             means = NULL,
-                             eqtl = NULL){
-    
+splatPopEstimate <- function(params = newSplatPopParams(), counts = NULL,
+                             means = NULL, eqtl = NULL) {
+
     checkmate::assertClass(params, "splatPopParams")
 
     # Estimate single-cell parameters using base splatEstimate function
-    # THIS DOESN'T WORK BECAUSE IT RETURNS A SPLATPARAM NOT A SPLATPOPPARAM...
-    #if(!is.null(counts)){params <- splatEstimate(counts)}
-    
+    if (!is.null(counts)) {
+        params <- splatEstimate(counts, params)
+    }
+
     # Get parameters for eQTL Effect Size distribution
-    if(!is.null(eqtl)){params <- splatPopEstimateEffectSize(params, eqtl)}
+    if (!is.null(eqtl)) {
+        params <- splatPopEstimateEffectSize(params, eqtl)
+    }
 
     # Get parameters for population wide gene mean and variance distributions
-    if(!is.null(means)){params <- splatPopEstimateMeanCV(params, means)}
-    
+    if (!is.null(means)) {
+        params <- splatPopEstimateMeanCV(params, means)
+    }
+
     return(params)
+
 }
 
 #' Estimate eQTL Effect Size parameters
@@ -47,29 +50,29 @@ splatPopEstimate <- function(params = newSplatPopParams(),
 #' Estimate rate and shape parameters for the gamma distribution used to
 #' simulate eQTL (eSNP-eGene) effect sizes.
 #'
-#' @param eqtl Data.frame with all or top eQTL pairs from a real eQTL analysis. 
-#'         Must include columns: gene_id, pval_nominal, and slope. 
-#' @param params splatPopParams object containing parameters for the 
+#' @param eqtl Data.frame with all or top eQTL pairs from a real eQTL analysis.
+#'         Must include columns: gene_id, pval_nominal, and slope.
+#' @param params splatPopParams object containing parameters for the
 #'         simulation of the mean expression levels for the population.
 #'        See \code{\link{splatPopParams}} for details.
 #'
 #' @details
 #' Parameters for the gamma distribution are estimated by fitting the top eSNP-
-#' eGene pair effect sizes using \code{\link[fitdistrplus]{fitdist}}. The 
-#' maximum goodness-of-fit estimation method is used to minimise the 
-#' Cramer-von Mises distance. This can fail in some situations, in which case 
-#' the method of moments estimation method is used instead. 
+#' eGene pair effect sizes using \code{\link[fitdistrplus]{fitdist}}. The
+#' maximum goodness-of-fit estimation method is used to minimise the
+#' Cramer-von Mises distance. This can fail in some situations, in which case
+#' the method of moments estimation method is used instead.
 #'
 #' @return params object with estimated values.
-#' 
+#'
 splatPopEstimateEffectSize <- function(params, eqtl) {
 
     # Test input eSNP-eGene pairs
-    if (!("gene_id" %in% names(eqtl) & 
-          "pval_nominal" %in% names(eqtl) & 
+    if (!("gene_id" %in% names(eqtl) &
+          "pval_nominal" %in% names(eqtl) &
           "slope" %in% names(eqtl))) {
         stop("Incorrect format for eqtl data.")}
-    
+
     # Select top eSNP for each gene (i.e. lowest p.value)
     eqtl.top <- eqtl[order(eqtl$gene_id, eqtl$pval_nominal), ]
     eqtl.top <- eqtl.top[!duplicated(eqtl.top$gene_id), ]
@@ -97,38 +100,38 @@ splatPopEstimateEffectSize <- function(params, eqtl) {
 #' estimated parameters are added to the params object. See
 #' \code{\link[fitdistrplus]{fitdist}} for details on the fitting.
 #'
-#' @param params splatPopParams object containing parameters for the 
+#' @param params splatPopParams object containing parameters for the
 #'         simulation of the mean expression levels for the population.
 #'        See \code{\link{splatPopParams}} for details.
-#' @param means Dataframe of real gene means across a population, where 
+#' @param means Dataframe of real gene means across a population, where
 #'        each row is a gene and each column is an individual in the population.
 #'
 #' @details
 #' Parameters for the mean gamma distribution are estimated by fitting the mean
 #' (across the population) expression of genes that meet the criteria (<50% of
-#' samples have exp <0.1) and parameters for the cv gamma distribution are 
+#' samples have exp <0.1) and parameters for the cv gamma distribution are
 #' estimated for each bin of mean expression using the cv of expression across
-#' the population for genes in that bin. Both are fit using 
+#' the population for genes in that bin. Both are fit using
 #' \code{\link[fitdistrplus]{fitdist}}. The "Nelder-Mead" method is used to fit
-#' the mean gamma distribution and the maximum goodness-of-fit estimation 
-#' method is used to minimise the Cramer-von Mises distance for the CV 
+#' the mean gamma distribution and the maximum goodness-of-fit estimation
+#' method is used to minimise the Cramer-von Mises distance for the CV
 #' distribution.
-#' 
+#'
 #' @return params object with estimated values.
 #' @importFrom stats quantile
 #' @importFrom grDevices boxplot.stats
 #' @importFrom matrixStats rowMedians
-#' 
+#'
 splatPopEstimateMeanCV <- function(params, means) {
-    
+
     # Test input gene means
     if ((anyNA(means) | !(validObject(rowSums(means))))) {
         stop("Incorrect format or NAs present in gene.means. See example data.")
     }
-    
+
     # Remove genes with low variance/low means
     abv.thr <- data.frame(perc = (rowSums(means >= 0.1)/ncol(means)))
-    
+
     means.use <- means[abv.thr$perc > 0.5, ]
 
     # Calculate mean expression parameters
@@ -136,40 +139,40 @@ splatPopEstimateMeanCV <- function(params, means) {
     names(row.means) <- row.names(means.use)
     mfit <- fitdistrplus::fitdist(row.means, "gamma",
                                   optim.method = "Nelder-Mead")
-    
+
     # Calculate CV parameters for genes based on 10 mean expression bins
     nbins <- getParam(params, "pop.cv.bins")
-    bins <- split(row.means, cut(row.means, quantile(row.means,(0:nbins)/nbins), 
+    bins <- split(row.means, cut(row.means, quantile(row.means,(0:nbins)/nbins),
                              include.lowest = TRUE))
     cvparams <- data.frame(start = character(), end = character(),
-                           shape = character(), rate = character(), 
+                           shape = character(), rate = character(),
                            stringsAsFactors = FALSE)
 
     for(b in names(bins)){
         re.brack.paren <- "\\[|\\]|\\)|\\("
         min.max <- strsplit(gsub(re.brack.paren, "", unlist(b)), split = ",")
-        
+
         b.gene.means <- means.use[row.means > as.numeric(min.max[[1]][1]) &
                                       row.means < as.numeric(min.max[[1]][2]), ]
-        
+
         cv <- apply(b.gene.means, 1, co.var)
         cv[is.na(cv)] <- 0
         cv <- cv[!cv %in% boxplot.stats(cv)$out]
         cvfit <- fitdistrplus::fitdist(cv, "gamma", method = "mge", gof = "CvM")
-        cvparams <- rbind(cvparams, 
+        cvparams <- rbind(cvparams,
                           list(start = as.numeric(as.numeric(min.max[[1]][1])),
-                               end = as.numeric(as.numeric(min.max[[1]][2])), 
+                               end = as.numeric(as.numeric(min.max[[1]][2])),
                                shape = cvfit$estimate["shape"],
                                rate = cvfit$estimate["rate"]),
                           stringsAsFactors = FALSE)
     }
-    
+
     cvparams[1, "start"] <- 0
     cvparams[nrow(cvparams), "end"] <- 1e100
-    params <- setParams(params, 
+    params <- setParams(params,
                         pop.mean.shape = unname(mfit$estimate["shape"]),
                         pop.mean.rate = unname(mfit$estimate["rate"]),
                         pop.cv.param = cvparams)
-    
+
     return(params)
 }
