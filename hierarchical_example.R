fa2332d9be28e361cb3608befb157d3182ab02b3
Keshav Motwani <keshavmot2@gmail.com>
2021-08-19 14:11:32 -0400

    update example

--- hierarchical_example.R
+++ hierarchical_example.R
@@ -2,22 +2,24 @@ library(splatter)
 library(scran)
 library(scater)
 
-params = newSplatParams(nGenes = 500,
-                        batchCells = rep(500, 2),
-                        batch.facLoc = 0.05,
-                        batch.facScale = 0.05,
-                        batch.rmEffect = FALSE,
-                        splits.per.level = c(4, 2, 2),
-                        de.prob.per.level = c(0.075, 0.05, 0.025),
-                        de.facLoc.per.level = 0.1,
-                        de.facScale.per.level = 0.4,
-                        seed = 3)
+params = newSplatParams()
+params = setParams(params,
+                   nGenes = 500,
+                   batchCells = rep(1000, 2),
+                   batch.facLoc = 0.05,
+                   batch.facScale = 0.05,
+                   batch.rmEffect = FALSE,
+                   splits.per.level = c(4, 2, 2),
+                   de.prob.per.level = c(0.05, 0.0333, 0.0167),
+                   de.facLoc.per.level = 0.1,
+                   de.facScale.per.level = 0.4,
+                   seed = 1)
 
 data = splatSimulate(params, method = "hierarchical", verbose = FALSE)
 
 data = logNormCounts(data)
 data = runPCA(data)
-data = runUMAP(data, spread = 10)
+data = runUMAP(data)
 
 plot(hclust(dist(t(as.matrix(rowData(data)[, grepl("DEFacGroup", colnames(rowData(data))), drop = FALSE])))))
 
