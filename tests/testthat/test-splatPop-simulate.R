82700d523ef3a42a0bf3c2ab5ad2ed952773e393
Luke Zappia <lazappi@users.noreply.github.com>
2022-10-25 14:30:14 +0200

    Add test for different cell numbers in splatPopSimulate()
    When nCells.sample=TRUE

--- tests/testthat/test-splatPop-simulate.R
+++ tests/testthat/test-splatPop-simulate.R
@@ -3,11 +3,12 @@ context("splatPopSimulate")
 if (requireNamespace("VariantAnnotation", quietly = TRUE) &&
     requireNamespace("preprocessCore", quietly = TRUE)) {
     set.seed(1)
-    
+
     n.samples <- 6
     n.genes <- 10
     vcf <- mockVCF(n.samples = n.samples, n.snps = 1000)
-    
+    gff <- mockGFF(n.genes = n.genes)
+
     params <- setParams(newSplatPopParams(), eqtl.n = 10, nGenes = n.genes)
 }
 
@@ -24,20 +25,18 @@ test_that("splatPopSimulate output is valid and works", {
                                                params = params)))
 })
 
-
 test_that("splatPopSimulate on multiple groups output is valid and works", {
     skip_if_not_installed("VariantAnnotation")
     skip_if_not_installed("preprocessCore")
     params.g2 <- setParams(params, group.prob = c(0.5, 0.5), nGenes = n.genes)
     eqtl <- splatPopSimulate(params = params.g2, vcf = vcf)
     expect_true(validObject(eqtl))
-    
+
 })
 
 test_that("splatPopSimulate can read genes from gff data.frame", {
     skip_if_not_installed("VariantAnnotation")
     skip_if_not_installed("preprocessCore")
-    gff <- mockGFF(n.genes = n.genes)
     sim <- splatPopSimulateMeans(vcf = vcf, params = params, gff = gff)
     expect_true(validObject(sim))
 })
@@ -46,10 +45,22 @@ test_that("splatPopSimulate can simulate from empirical data directly", {
     skip_if_not_installed("VariantAnnotation")
     skip_if_not_installed("preprocessCore")
     emp <- mockEmpiricalSet(seed=1)
-    
-    sim <- splatPopSimulateMeans(vcf = emp$vcf, gff = emp$gff, eqtl = emp$eqtl, 
-                             means = emp$means)
 
-    expect_true(validObject(sim))   
-    
+    sim <- splatPopSimulateMeans(vcf = emp$vcf, gff = emp$gff, eqtl = emp$eqtl,
+                                 means = emp$means)
+
+    expect_true(validObject(sim))
+})
+
+test_that("splatPopSimulate with nCells.sample gives different cell counts", {
+    skip_if_not_installed("VariantAnnotation")
+    skip_if_not_installed("preprocessCore")
+
+    params.nCells <- newSplatPopParams(nCells.sample = TRUE, nCells.shape = 1.5,
+                                       nCells.rate = 0.01)
+
+    sim <- splatPopSimulate(vcf = vcf, gff = gff, params = params.nCells)
+    cell.counts <- table(colData(sim)$Sample)
+
+    expect_false(all(cell.counts == cell.counts[1]))
 })
