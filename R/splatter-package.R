411b3faebc22848da564d6b228545841e175b366
Luke Zappia <lazappi@users.noreply.github.com>
2024-03-26 14:38:13 +0100

    Replace package man page with usethis auto page

--- R/splatter-package.R
+++ R/splatter-package.R
@@ -1,15 +1,6 @@
-#' splatter.
-#'
-#' \pkg{splatter} is a package for the well-documented and reproducible
-#' simulation of single-cell RNA-seq count data.
-#'
-#' As well as it's own simulation model \pkg{splatter} provides functions for
-#' the estimation of model parameters.
-#'
-#' @seealso
-#' Zappia L, Phipson B, Oshlack A. Splatter: Simulation Of Single-Cell RNA
-#' Sequencing Data. bioRxiv. 2017; doi:10.1101/133173
-#'
-#' @name splatter
-#' @docType package
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
