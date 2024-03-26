0454898d63cc7f1a2ed5c45e5726029f063b8ba0
Luke Zappia <lazappi@users.noreply.github.com>
2024-03-26 14:17:54 +0100

    Set verbose=FALSE in tests for scDDSimulate()

--- tests/testthat/test-scDD-simulate.R
+++ tests/testthat/test-scDD-simulate.R
@@ -4,5 +4,5 @@ test_that("scDD simulation output is valid", {
     skip_if_not_installed("scDD")
     params <- newSCDDParams(nDE = 5, nDP = 5, nDM = 5, nDB = 5, nEE = 5,
                             nEP = 5)
-    expect_true(validObject(scDDSimulate(params)))
+    expect_true(validObject(scDDSimulate(params, verbose = FALSE)))
 })
